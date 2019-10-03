15f4f1f05e9b15a09843aacbdc663a769c9f4ec7
Peter Mangelsdorf <pjm349@drexel.edu>
2019-10-03 08:51:30 -0400

    Ermagerd was it worth it???

---
+++ Rag-Ball/Assets/_RagBall/Scripts/PlayerAttack.cs
@@ -0,0 +1,117 @@
+﻿
+
+//Description
+/* PlayerAttack uses PlayerHealth to interact with other players.
+ * The player does not necessarily attack, no damage is dealt.
+ * Instead, messages are passed and the physics system takes over.
+ * This script needs a player, target, and health scripts for the player and target.
+ */
+
+
+//Declarations
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+//PlayerAttack Class
+public class PlayerAttack : MonoBehaviour
+{
+
+
+    //Temporary Variables
+    private float AttackTimer;
+    public float AttackSpeed = 0.5f;
+    public Transform AttackDirection;
+    bool ThingInRange;
+
+
+    //Messaging Variables
+    GameObject Target;
+    Rigidbody TargetRigidbody;
+    PlayerHealth ThisPlayerHealth;
+    PlayerMove ThisPlayerMove;
+
+
+    //Stats Variables
+    private float ThrowForce = 60f;
+    private float HoldTime;
+    private float HoldTimer;
+
+
+
+    private GameObject collidedObject = null;
+
+    //Awake
+    void Awake ()
+    {
+        //TEMP: Reset AttackTimer
+        AttackTimer = 0f;
+
+        //Reset HoldTimer
+        HoldTimer = 0f;
+
+        //Find Stats
+        ThisPlayerHealth = GetComponent<PlayerHealth>();
+
+    }
+
+
+    //Update
+    void Update ()
+    {
+        //Check for what player wants to do
+
+        //Player wants to grab and is in range
+        if (Input.GetMouseButtonDown(1) && ThingInRange)
+        {
+            Grab ();
+        }
+
+        //Player wants to throw
+        else if (Input.GetMouseButtonDown(0))
+        {
+            Throw ();
+        }
+
+        collidedObject = Target;
+        if (collidedObject != null)
+        {
+            Vector3 offset = new Vector3(0, 0, 1);
+            transform.position = collidedObject.transform.position + offset;
+        }
+    }
+
+
+    //Grab
+    void Grab ()
+    {
+        TargetRigidbody = Target.GetComponent<Rigidbody>();
+
+    }
+
+
+    //Throw
+    void Throw ()
+    {
+        
+        TargetRigidbody.AddForce(transform.forward * ThrowForce);
+    }
+
+
+    //ONTRIGGERENTER
+    //Checks if something in range is the player
+    void OnTriggerEnter(Collider Other)
+    {
+        if (Other.GetComponent<Collider>().gameObject.layer == 8 || Other.GetComponent<Collider>().gameObject.layer == 9)
+        { 
+            ThingInRange = true;
+            Target = Other.GetComponent<Collider>().gameObject;
+            Debug.Log("Hit something");
+        }
+    }
+
+
+
+}
+
---
+++ Rag-Ball/Assets/_RagBall/Scripts/PlayerMove.cs
@@ -0,0 +1,129 @@
+﻿
+
+//Description
+/* PlayerMove translates and rotates the player model on the game world.
+ * 
+ * It needs a player model with rigidbody,
+ * and a Plane for the player to stand on.
+ * 
+ * Also includes special abilities.
+ * Jumping and Dashing
+ */
+
+
+//Declarations
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+//PlayerMove Class
+public class PlayerMove : MonoBehaviour
+{
+
+
+    //Variables
+    public float MoveSpeed = 6f;
+    Vector3 Movement;
+    Rigidbody PlayerRigidbody;
+    public int Floor;
+    float CameraRayLength = 100f;
+    PlayerHealth ThisPlayerHealth;
+
+
+    //Awake
+    void Awake ()
+    {
+        PlayerRigidbody = GetComponent<Rigidbody>();
+        ThisPlayerHealth = GetComponent<PlayerHealth>();
+    }
+
+
+    //FixedUpdate
+    void FixedUpdate ()
+    {
+        //Raw Input for Snappy Movement
+        //Horizontal maps to the "A" and "D" keys
+        //Vertical maps to the "W" and "S" keys
+        float H = Input.GetAxisRaw("Horizontal");
+        float V = Input.GetAxisRaw("Vertical");
+
+        //TEMP: Detect if jump requested and available
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Jump ();
+        }
+
+        //TEMP: Detect if Dash requested and available
+        if (Input.GetKeyDown(KeyCode.LeftShift) && ThisPlayerHealth.Stamina > 0)
+        {
+            Dash ();
+        }
+
+        //Separate Functions for modification
+        Move (H, V);
+        Turn ();
+    }
+
+
+    //Move
+    void Move (float H, float V)
+    {
+        //Point Vector along X, Y Input
+        Movement.Set(H, 0f, V);
+
+        //Set Magnitude per frame at the correct Speed and magnitude
+        Movement = Movement.normalized * MoveSpeed * Time.deltaTime;
+        
+        //Apply Vector to Player at Player's position
+        PlayerRigidbody.MovePosition(transform.position + Movement);
+    }
+
+
+    //Turn
+    void Turn ()
+    {
+        //Creates and binds a ray to the Mouse
+        Ray CameraRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+        
+        //Gives information to other scripts
+        RaycastHit FloorHit;
+
+        //Casts the ray and checks if it hits
+        if (Physics.Raycast(CameraRay, out FloorHit, CameraRayLength, Floor))
+        {
+            //Distance from player to aimpoint
+            Vector3 PlayerToMouse = FloorHit.point - transform.position;
+            
+            //Prevent Player from rotating up and down
+            PlayerToMouse.y = 0f;
+            
+            //Set direction of player to mouse
+            Quaternion NewRotation = Quaternion.LookRotation(PlayerToMouse);
+            
+            //Move the player model by the angle
+            PlayerRigidbody.MoveRotation(NewRotation);
+        }
+    }
+
+
+    //TEMP: Dash
+    //Uses regular Rigidbody manipulation
+    void Dash ()
+    {
+        //Apply force to player in direction of travel
+        PlayerRigidbody.AddForce(transform.forward * ThisPlayerHealth.DashSpeed);
+    }
+
+
+    //TEMP: Jump
+    //Uses regular Rigidbody manipulation
+    void Jump ()
+    {
+        //Apply force to player vertically
+        PlayerRigidbody.AddForce(transform.up * ThisPlayerHealth.DashSpeed);
+    }
+
+
+}
+
