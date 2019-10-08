e071a4358a6d510e724711c4452c568c087b16ff
Peter Mangelsdorf <pjm349@drexel.edu>
2019-10-08 19:16:30 -0400

    ERMAGHERD

---
+++ Assets/_RagBall/Scripts/TriggerRespawn.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerRespawn : MonoBehaviour
+{
+    public Transform RespawnPosition;
+
+    void OnCollisionEnter(Collision collision)
+    {
+        collision.gameObject.transform.position = RespawnPosition.position;
+    }
+}
