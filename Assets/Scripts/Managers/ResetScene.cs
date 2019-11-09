c1182f8acd7f5f9a0b5ab4b82736e08d8ef01015
Arkae-RK <52971189+Arkae-RK@users.noreply.github.com>
2019-11-09 11:20:19 -0500

    Added Reset Scene to Build - Austin

---
+++ Assets/Scripts/Managers/ResetScene.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ResetScene : MonoBehaviour
+{
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            SceneManager.LoadScene(0);
+        }
+    }
+}
