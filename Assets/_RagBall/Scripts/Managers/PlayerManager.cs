412473da16ef074089830412fae89e37669becc9
Peter Mangelsdorf <pjm349@drexel.edu>
2019-11-14 08:41:52 -0500

    THROW FORCE IS NOT ENOUGH??????

--- Assets/_RagBall/Scripts/Managers/PlayerManager.cs
+++ Assets/_RagBall/Scripts/Managers/PlayerManager.cs
@@ -402,7 +402,7 @@ public class PlayerManager : MonoBehaviour {
         tharHips.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.None;
 
         // Applicaticize this here force on thad thar fella's pelvis
-        tharHips.GetComponent<Rigidbody>().AddForce(maHips.transform.forward * 6000f);
+        tharHips.GetComponent<Rigidbody>().AddForce(maHips.transform.forward * 10000f);
 
         // Reset thar grabblerability
         theirGrabbable.iCanGrab = true;
