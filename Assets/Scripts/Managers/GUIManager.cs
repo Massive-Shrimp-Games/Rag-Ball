f7c5fc3852e2284c8be5226ca502c9aa1d2454e7
Austin <austinabrams1999@gmail.com>
2019-11-14 00:04:39 -0500

    Pause Menus Implementation

---
+++ Assets/Scripts/Managers/GUIManager.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class GUIManager : MonoBehaviour
+{
+    public void Resume()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void Controls()
+    {
+        
+    }
+
+    public void Options()
+    {
+        
+    }
+
+    public void Quit()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
