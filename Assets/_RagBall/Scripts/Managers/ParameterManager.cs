ee0e620d57788da1d2a124613544301c2b97f526
Austin <austinabrams1999@gmail.com>
2019-11-17 19:01:32 -0500

    Working on sliders

---
+++ Assets/_RagBall/Scripts/Managers/ParameterManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParameterManager : MonoBehaviour
+{
+    public GameObject RedPipeParent;
+    public GameObject BluePipeParent;
+    public float PipeScale = 0.05f;
+
+    public void PipeScaleChanged(float s)
+    {
+        Debug.Log("SCALING: " + s);
+        PipeScale = s;
+        RedPipeParent.transform.localScale = new Vector3(PipeScale, PipeScale, PipeScale);
+        BluePipeParent.transform.localScale = new Vector3(PipeScale, PipeScale, PipeScale);
+    }
+
+
+    
+    
+}
