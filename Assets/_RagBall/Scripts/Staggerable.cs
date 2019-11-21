7951dc4e37a82087b92c5d924882601c8b65bb93
Peter Mangelsdorf <pjm349@drexel.edu>
2019-11-21 01:40:03 -0500

    Added vars to staggerable, research notes

--- Assets/_RagBall/Scripts/Staggerable.cs
+++ Assets/_RagBall/Scripts/Staggerable.cs
@@ -4,9 +4,32 @@ using UnityEngine;
 
 public class Staggerable : MonoBehaviour
 {
+    // VARIABLES
 
+    // Logic
     public bool iCanStagger;
 
+    // Torso
+    public CharacterJoint Spine_CJ;
+    public CharacterJoint Head_CJ;
+
+    // Left
+    public ConfigurableJoint ThighL_CJ;
+    public ConfigurableJoint ShinL_CJ;
+
+    // Right
+    public ConfigurableJoint ThighR_CJ;
+    public ConfigurableJoint ShinR_CJ;
+
+
+
+    // NOTES
+
+    // https://forum.unity.com/threads/enable-disable-a-joint.24525/
+    // https://forum.unity.com/threads/configurable-joints-in-depth-documentation-tutorial-for-dummies.389152/
+    // https://docs.unity3d.com/Manual/class-ConfigurableJoint.html
+    // https://docs.unity3d.com/Manual/class-CharacterJoint.html
+    // https://answers.unity.com/questions/868898/make-gameobject-always-vertical-to-terrain.html
 
 
 }
