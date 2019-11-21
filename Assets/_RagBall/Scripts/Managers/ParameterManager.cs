9907827fc23f07af091feeb0df27f096bf206b68
Peter Mangelsdorf <pjm349@drexel.edu>
2019-11-21 07:21:48 -0500

    Solved the mystery of the disappearing goals. God, I oughta write a mystery novel

--- Assets/_RagBall/Scripts/Managers/ParameterManager.cs
+++ Assets/_RagBall/Scripts/Managers/ParameterManager.cs
@@ -1,17 +1,29 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+//using UnityEngine.UI.Selectable;
 
 public class ParameterManager : MonoBehaviour
 {
     public GameObject RedPipeParent;
     public GameObject BluePipeParent;
     public float PipeScale = 0.05f;
+    public Slider daddySlider;
+
+
 
     public void PipeScaleChanged(float s)
     {
         Debug.Log("SCALING: " + s);
         PipeScale = s;
+        RedPipeParent.transform.localScale = new Vector3(PipeScale, PipeScale, PipeScale);
+        BluePipeParent.transform.localScale = new Vector3(PipeScale, PipeScale, PipeScale);
+    }
+
+    public void Update()
+    {
+        PipeScale = daddySlider.value;
         RedPipeParent.transform.localScale = new Vector3(PipeScale, PipeScale, PipeScale);
         BluePipeParent.transform.localScale = new Vector3(PipeScale, PipeScale, PipeScale);
     }
