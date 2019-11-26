1ee7e57f078e9f43c9a5d540a7a36628c04e698a
Peter Mangelsdorf <pjm349@drexel.edu>
2019-11-26 15:44:28 -0500

    Setting up Scripts for Refactoring (will make parameterization easier)

---
+++ Assets/_RagBall/Scripts/Managers/GameManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Manages gameplay objects in the scene
+/// Players, Goals, Score, Effects
+/// </summary>
+public class GameManager : MonoBehaviour
+{
+
+
+
+
+
+
+
+    /// <summary>
+    /// Sets Default Values
+    /// </summary>
+    void Start()
+    {
+        
+    }
+
+
+
+    /// <summary>
+    /// Update Values
+    /// </summary>
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/_RagBall/Scripts/Settings/Setting.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Setting is an Abstract Class to define all other settings
+/// This is a Singleton pattern
+/// Each setting should affect ONE and only ONE thing
+/// If a setting gets too large, BREAK IT UP, or expand it into a FULL FEATURE
+/// </summary>
+public abstract class Setting : MonoBehaviour
+{
+
+    /// <summary>
+    /// A Setting must have a SINGLE PRIMARY parameter which it changes
+    /// </summary>
+    private Object mySetting;
+
+    /// <summary>
+    /// A setting must set its default values
+    /// </summary>
+    public Setting()
+    {
+
+    }
+
+
+}
