9f4d5af860e5710f76f6b0e3cfc6425bfe29ee78
Abrams <ata57@drexel.edu>
2019-12-10 22:29:44 -0500

    Char select -A

---
+++ Assets/_RagBall/Scripts/Managers/CS_Screen.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CS_Screen : MonoBehaviour
+{
+    public Canvas LetsGo;
+
+    void Start()
+    {
+    	LetsGo.enabled = false;
+    }
+
+    void Update()
+    {
+        if (LetsGo.enabled && (Input.GetButtonDown("A1") || Input.GetButtonDown("A2") || Input.GetButtonDown("A3") || Input.GetButtonDown("A4")))
+        {
+            SceneManager.LoadScene(2);
+        }
+
+        if (LetsGo.enabled && (Input.GetButtonDown("B1") || Input.GetButtonDown("B2") || Input.GetButtonDown("B3") || Input.GetButtonDown("B4")))
+        {
+            LetsGo.enabled = false;
+        }
+
+        if (Input.GetButtonDown("Start1") || Input.GetButtonDown("Start2") || Input.GetButtonDown("Start3") || Input.GetButtonDown("Start4"))
+        {
+            LetsGo.enabled = true;
+        }
+    }
+}
