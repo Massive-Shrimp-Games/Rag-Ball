e769c9681581ce122800c8fb6a637f421330c9ac
Abrams <ata57@drexel.edu>
2019-12-10 23:02:42 -0500

    Added Char select? and Level select almost. New ONLY court scene made -A

---
+++ Assets/_RagBall/Scripts/Managers/LevelSelect.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelSelect : MonoBehaviour
+{
+    
+    public Canvas CourtLevel;
+    public Canvas LedgeLevel;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
