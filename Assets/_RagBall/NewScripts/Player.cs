6911f4cd379a079ce0cd3cd68306cbf6c9c31a46
ccp49 <46506434+ccp49@users.noreply.github.com>
2020-01-18 13:55:30 -0500

    readded grab/drop

--- Assets/_RagBall/NewScripts/Player.cs
+++ Assets/_RagBall/NewScripts/Player.cs
@@ -57,6 +57,7 @@ public class Player : MonoBehaviour
     private void Update()
     {
         //movement = Vector2.zero;
+        UpdateHeld();
     }
 
     void Start()
@@ -118,7 +119,7 @@ public class Player : MonoBehaviour
         Vector2 stickDirection = inputValue.Get<Vector2>();
         Vector3 force = new Vector3(stickDirection.x, 0, stickDirection.y) * playerSpeed * Time.deltaTime;
         hips.GetComponent<Rigidbody>().AddForce(force);
-        Debug.LogFormat("stickDir is {0}", stickDirection);
+        //Debug.LogFormat("stickDir is {0}", stickDirection);
         if (Mathf.Abs(stickDirection.x) >= 0.1 || Mathf.Abs(stickDirection.y) >= 0.1)
         {
             hips.transform.forward = new Vector3(stickDirection.x, 0, stickDirection.y);
