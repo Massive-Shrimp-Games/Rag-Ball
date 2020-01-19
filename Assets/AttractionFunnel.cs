facfc217440ce3522c430891a1fd21ca449f77ca
Scott-Nat <stn38@drexel.edu>
2020-01-18 19:00:39 -0500

    Created The WindFunnel Prefab
    Created 2 scripts and setup all the variables.
    Created the Prefaab WindFunnel.

---
+++ Assets/AttractionFunnel.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttractionFunnel : MonoBehaviour
+{
+    public GameObject FunnelPoint;
+    [SerializeField] private float magnitude;
+    void OnTriggerStay(Collider col)
+    {
+        Rigidbody colRigidbody = col.GetComponent<Rigidbody>();
+        Vector3 directionDraw = FunnelPoint.transform.position - colRigidbody.position;
+        Vector3 drawForce = directionDraw.normalized * magnitude;
+
+        if (colRigidbody != null)
+        {
+            colRigidbody.AddForce(drawForce);
+        }
+    }
+}
