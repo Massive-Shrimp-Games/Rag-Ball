a2f91eebcc1338df59f2fd05692095d017c00726
Ranson <jrr334@drexel.edu>
2020-01-18 22:52:16 -0500

    Added script to switch to preload scene on play in editor

---
+++ Assets/Editor/DefaultSceneLoader.cs
@@ -0,0 +1,29 @@
+﻿#if UNITY_EDITOR
+using UnityEditor;
+using UnityEditor.SceneManagement;
+
+[InitializeOnLoadAttribute]
+public static class DefaultSceneLoader
+{
+
+    static DefaultSceneLoader()
+    {
+        EditorApplication.playModeStateChanged += LoadDefaultScene;
+    }
+
+    static void LoadDefaultScene(PlayModeStateChange state)
+    {
+        if (state == PlayModeStateChange.ExitingEditMode)
+        {
+            EditorSceneManager.SaveCurrentModifiedScenesIfUserWantsTo();
+        }
+
+        if (state == PlayModeStateChange.EnteredPlayMode)
+        {
+            int currentSceneBuildIndex = EditorSceneManager.GetActiveScene().buildIndex;
+            EditorSceneManager.LoadScene(0);
+            EditorSceneManager.LoadScene(currentSceneBuildIndex);
+        }
+    }
+}
+#endif
