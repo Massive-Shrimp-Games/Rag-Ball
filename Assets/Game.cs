0ee240458454db39add816c915f0c2fdbe2f5a1a
Ranson <jrr334@drexel.edu>
2020-01-18 22:52:58 -0500

    Added preload scene and game singleton script

---
+++ Assets/Game.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Game : Singleton<Game>
+{
+    public Scene scene;
+    private void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+
+    private void Start()
+    {
+#if !UNITY_EDITOR
+        UnityEngine.SceneManagement.SceneManager.LoadScene(1);
+#endif
+    }
+}
