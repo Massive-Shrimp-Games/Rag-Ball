3c5a447404510907ba76cb5fa87737799c14e6eb
Morrison <fjm52@drexel.edu>
2020-01-31 17:23:40 -0500

    Toggle for switch and fan

---
+++ Assets/LeverToggle.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LeverToggle : MonoBehaviour
+{
+    [SerializeField]    private float triggerSpeed = 5f;
+    private float incomingSpeed;
+    public Material enabledMat;
+    public Material disabledMat;
+
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        incomingSpeed = collision.relativeVelocity.magnitude;
+        
+        if (incomingSpeed >= triggerSpeed)
+        {
+            TriggerActivate();
+        }
+    }
+
+    private void TriggerActivate()
+    {
+        if (GameObject.Find("WindFunnel").GetComponent<WindFunnelController>().WindFunnelToggle)
+        {
+            GameObject.Find("PinLever_Model_01").transform.Rotate(0, 180, 0);
+            GameObject.Find("InnerCylinder").GetComponent<MeshRenderer>().material = disabledMat;
+            GameObject.Find("WindFunnel").GetComponent<WindFunnelController>().WindFunnelToggle = false;
+        }
+        if (GameObject.Find("WindFunnel").GetComponent<WindFunnelController>().WindFunnelToggle == false)
+        {
+            GameObject.Find("PinLever_Model_01").transform.Rotate(0, 180, 0);
+            GameObject.Find("InnerCylinder").GetComponent<MeshRenderer>().material = enabledMat;
+            GameObject.Find("WindFunnel").GetComponent<WindFunnelController>().WindFunnelToggle = true;
+        }
+    }
+}
---
+++ Assets/WindFunnelController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WindFunnelController : MonoBehaviour
+{
+    public bool WindFunnelToggle = false;
+    public float speed = 10f;
+    public GameObject WindFunnelHolder;
+    
+    void Update()
+    {
+        if (WindFunnelToggle == false)
+        {
+            transform.Rotate(Vector3.up * 0);
+            WindFunnelHolder.SetActive(false);
+        }
+
+        if (WindFunnelToggle == true)
+        {
+            transform.Rotate(Vector3.up * speed * Time.deltaTime);
+            WindFunnelHolder.SetActive(true);
+        }
+    }
+}
