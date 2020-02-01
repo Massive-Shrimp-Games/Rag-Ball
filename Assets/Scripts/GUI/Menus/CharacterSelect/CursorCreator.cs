6e8f862e507fa6f116ab021d7e33f70b7646fe95
Ranson <jrr334@drexel.edu>
2020-01-31 22:09:19 -0500

    Character select screen creates cursors for each controller

--- Assets/Scripts/GUI/Menus/CharacterSelect/CursorCreator.cs
+++ Assets/Scripts/GUI/Menus/CharacterSelect/CursorCreator.cs
@@ -2,20 +2,18 @@
 
 public class CursorCreator : MonoBehaviour
 {
-    public GameObject parentCanvas;
     public MenuItem defaultItem;
-    public GameObject[] cursorPrefabs;
+    public GameObject cursorPrefab;
 
     void Start()
     {
         if (Game.Instance == null) return;
         for (int i = 0; i < Game.Instance.Controllers.Count(); i++)
         {
-            GameObject c = Instantiate(cursorPrefabs[i]);
-            c.GetComponent<PlayerCursor>().currentMenuItem = defaultItem;
-            c.GetComponent<PlayerCursor>().BindController(i);
-            c.transform.parent = parentCanvas.transform;
-            c.GetComponent<PlayerCursor>().currentMenuItem = defaultItem;
+            PlayerCursor cursor = Instantiate(cursorPrefab).GetComponent<PlayerCursor>();
+            cursor.transform.parent = transform.parent;
+            cursor.currentMenuItem = defaultItem;
+            cursor.BindController(i);
         }
     }
 }
