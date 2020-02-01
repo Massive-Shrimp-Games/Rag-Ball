6cf71f3ed09b71e315867a89c5e9744cf5457d22
Austin <austinabrams1999@gmail.com>
2020-01-31 23:43:27 -0500

    Trail added to back of ragdoll

---
+++ Assets/TrailRend.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrailRend : MonoBehaviour
+{
+    [SerializeField] private float triggerSpeed = 15f;
+    private float movementSpeed;
+
+
+    private void update()
+    {
+    	movementSpeed = gameObject.transform.parent.GetComponent<Rigidbody>().velocity.magnitude;
+
+    	//GetComponent<Rigidbody>.velocity;
+
+    	if (movementSpeed >= triggerSpeed)
+    	{
+    		GetComponent<TrailRenderer>().enabled = true;
+    	}
+    }
+}
