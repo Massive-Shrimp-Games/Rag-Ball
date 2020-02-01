305e7280e18b30c2c6aec9d8f43b533696ef592f
Ranson <jrr334@drexel.edu>
2020-01-31 21:17:40 -0500

    Hooked up character select screen

---
+++ Assets/Scripts/GUI/Menus/CharacterSelect/CharacterButton.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterButton : Button
+{
+    public override void Select(PlayerCursor cursor)
+    {
+        throw new System.NotImplementedException();
+    }
+}
---
+++ Assets/Scripts/GUI/Menus/CharacterSelect/RandomCharacterButton.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomCharacterButton : Button
+{
+    public override void Select(PlayerCursor cursor)
+    {
+        throw new System.NotImplementedException();
+    }
+}
