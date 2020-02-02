1e50472d403a7920384e5ce75ebf592eabefcc43
Abrams <ata57@drexel.edu>
2020-02-01 21:38:22 -0500

    Finalized look of trail but Code for it still isnt working but it looks good no matter what

--- Assets/TrailRend.cs
+++ Assets/TrailRend.cs
@@ -4,19 +4,22 @@ using UnityEngine;
 
 public class TrailRend : MonoBehaviour
 {
-    [SerializeField] private float triggerSpeed = 15f;
-    private float movementSpeed;
-
+    [SerializeField] private float triggerSpeed = 20f;
+    [SerializeField] private float movementSpeed;
+    public GameObject Trail;
 
     private void update()
     {
     	movementSpeed = gameObject.transform.parent.GetComponent<Rigidbody>().velocity.magnitude;
-
-    	//GetComponent<Rigidbody>.velocity;
-
+        Debug.Log("My speed is" + movementSpeed);
+    	
     	if (movementSpeed >= triggerSpeed)
     	{
-    		GetComponent<TrailRenderer>().enabled = true;
-    	}
+            Trail.SetActive(true);
+        }
+        else
+        {
+            Trail.SetActive(false);
+        }
     }
 }
