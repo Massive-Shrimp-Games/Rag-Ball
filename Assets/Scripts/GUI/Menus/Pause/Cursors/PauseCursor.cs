3271095ef266794ae8d463473f03d20159c4e036
Ranson <jrr334@drexel.edu>
2020-02-07 12:46:23 -0500

    Fixed broken pause menu

---
+++ Assets/Scripts/GUI/Menus/Pause/Cursors/PauseCursor.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine.InputSystem;
+
+public class PauseCursor : PlayerCursor
+{
+    protected override void OnReturn(InputValue inputValue)
+    {
+        if (Game.Instance == null) return;
+        Game.Instance.PauseMenu.UnPause();
+    }
+
+    protected override void OnStart(InputValue inputValue)
+    {
+        if (Game.Instance == null) return;
+        Game.Instance.PauseMenu.UnPause();
+    }
+}
--- Assets/Scripts/GUI/Menus/Pause/PauseMenu.cs
+++ Assets/Scripts/GUI/Menus/Pause/PauseMenu.cs
@@ -8,17 +8,11 @@ public class PauseMenu : MonoBehaviour
     public bool paused { get; private set; }
     private Controller controller;
     private GameModeSelectCursor cursor;
-    public GameObject defaultItem;
-
-    private void Start()
-    {
-        cursor.currentMenuItem = defaultItem.GetComponent<ResumeButton>();
-    }
 
     public void Pause(Controller controller)
     {
         this.controller = controller;
-        //controller.GetComponent<PlayerInput>().SwitchCurrentActionMap("Menu");
+        // controller.GetComponent<PlayerInput>().SwitchCurrentActionMap("Menu");
         Time.timeScale = 0f;
         gameObject.SetActive(true);
         paused = true;
@@ -26,7 +20,7 @@ public class PauseMenu : MonoBehaviour
 
     public void UnPause()
     {
-        //controller.GetComponent<PlayerInput>().SwitchCurrentActionMap("Player");
+        // controller.GetComponent<PlayerInput>().SwitchCurrentActionMap("Player");
         this.controller = null;
         Time.timeScale = 1f;
         gameObject.SetActive(false);
