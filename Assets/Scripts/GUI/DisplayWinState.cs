de03e48b6fe76220c3ea9d392103d681a6096536
Abrams <ata57@drexel.edu>
2020-02-12 14:11:18 -0500

    Added camera move and continued polishing win state

--- Assets/Scripts/GUI/DisplayWinState.cs
+++ Assets/Scripts/GUI/DisplayWinState.cs
@@ -4,30 +4,57 @@ using UnityEngine;
 
 public class DisplayWinState : MonoBehaviour
 {
-    public GameObject RedWin;
-    public GameObject BlueWin;
-    public GameObject TieWin;
+    public GameObject RedWinText;
+    public GameObject BlueWinText;
+    public GameObject TieWinText;
+    public GameObject WinLeftPosition;
+    public GameObject WinRightPosition;
+    public GameObject LoseLeftPosition;
+    public GameObject LoseRightPosition;
 
     public void Start()
     {
-        RedWin.SetActive(false);
-        BlueWin.SetActive(false);
-        TieWin.SetActive(false);
+        RedWinText.SetActive(false);
+        BlueWinText.SetActive(false);
+        TieWinText.SetActive(false);
     }
 
     public void DisplayWinner()
     {
+        GameObject.Find("Stamina_Canvas").SetActive(false);
+        GameObject.Find("Main Camera").transform.position = new Vector3(0.55f, 4.62f, 1.19f);
+        GameObject.Find("Main Camera").transform.Rotate(-18.372f, 0f, 0f);
         if (RagballRuleset.redScore >= GameModeSelect.goalLimit && GameModeSelect.goalLimit != 0)
         {
-            RedWin.SetActive(true);
+            RedWinText.SetActive(true);
+            RedWin();
         }
         if (RagballRuleset.blueScore >= GameModeSelect.goalLimit && GameModeSelect.goalLimit != 0)
         {
-            BlueWin.SetActive(true);
+            BlueWinText.SetActive(true);
+            BlueWin();
         }
         if (RagballRuleset.blueScore == RagballRuleset.redScore && RagballRuleset.blueScore == GameModeSelect.goalLimit && GameModeSelect.goalLimit != 0)
         {
-            TieWin.SetActive(true);
+            TieWinText.SetActive(true);
+            TieWin();
         }
     }
+
+    private void RedWin()
+    {
+
+    }
+
+    private void BlueWin()
+    {
+
+    }
+
+    private void TieWin()
+    {
+
+    }
+
+
 }
