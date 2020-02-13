b60ba1ae04aae2bebf15fc38aab269072fbdddff
GriffinRobbins <griffin.a.robbins@gmail.com>
2020-02-12 21:56:41 -0500

    proof of concept for rag of the hill

---
+++ Assets/RagOfTheHill.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RagOfTheHill : MonoBehaviour
+{
+    public GameObject player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.tag == "RagDoll")
+        {
+            
+
+            if (player.GetComponent<Player>().playerNumber == 0)
+            {
+                Debug.Log("peeppee");
+            }
+        }
+    }
+}
