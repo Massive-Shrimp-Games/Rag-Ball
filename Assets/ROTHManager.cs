9bb81eeb8708cf5567d8c647e5757639cb8e0cdd
Robbins Lentz <gr397@drexel.edu>
2020-02-19 14:01:41 -0500

    Added more to ROTH

--- Assets/ROTHManager.cs
+++ Assets/ROTHManager.cs
@@ -6,7 +6,10 @@ using UnityEngine.UI;
 public class ROTHManager : MonoBehaviour
 {
     public int P1Score;
-    public text P1Text;
+    public int P2Score;
+    public int P3Score;
+    public int P4Score;
+    public Text P1Text;
     
     // Start is called before the first frame update
     void Start()
@@ -18,5 +21,25 @@ public class ROTHManager : MonoBehaviour
     void Update()
     {
         P1Text.text = P1Score.ToString();
+    }
+
+    public void P1AddScore(int ScoreValue)
+    {
+        P1Score += ScoreValue;
+    }
+
+    public void P2AddScore(int ScoreValue)
+    {
+        P2Score += ScoreValue;
+    }
+
+    public void P3AddScore(int ScoreValue)
+    {
+        P3Score += ScoreValue;
+    }
+
+    public void P4AddScore(int ScoreValue)
+    {
+        P3Score += ScoreValue;
     }
 }
--- Assets/RagOfTheHill.cs
+++ Assets/RagOfTheHill.cs
@@ -6,10 +6,19 @@ public class RagOfTheHill : MonoBehaviour
 {
     public GameObject player;
     public GameObject GameManager;
+
+    public float P1timer;
+    public float P2timer;
+    public float P3timer;
+    public float P4timer;
+
+    public int pointValue;
+
+    private float originalTimer;
     // Start is called before the first frame update
     void Start()
     {
-        
+         originalTimer = P1timer;
     }
 
     // Update is called once per frame
@@ -24,9 +33,102 @@ public class RagOfTheHill : MonoBehaviour
         {
             
 
-            if (player.GetComponent<Player>().playerNumber == 0)
+            if (player.GetComponent<Player>().playerNumber == 1)
             {
-                GameManager.GetComponent<ROTHManager>().P1Score +=1;
+
+                if (P1timer >= 0.0f)
+                {
+                    P1timer -= Time.deltaTime;
+                    int min = Mathf.FloorToInt(P1timer / 60);
+                    int sec = Mathf.FloorToInt(P1timer % 60);
+                    
+                }
+                if (P1timer <= 0.0f)
+                {
+                    P1timer = originalTimer;
+                    GameManager.GetComponent<ROTHManager>().P1AddScore(pointValue);
+                }           
+                  
+             }
+
+            if (player.GetComponent<Player>().playerNumber == 2)
+            {
+
+                if (P2timer >= 0.0f)
+                {
+                    P1timer -= Time.deltaTime;
+                    int min = Mathf.FloorToInt(P2timer / 60);
+                    int sec = Mathf.FloorToInt(P2timer % 60);
+
+                }
+                if (P2timer <= 0.0f)
+                {
+                    P2timer = originalTimer;
+                    GameManager.GetComponent<ROTHManager>().P2AddScore(pointValue);
+                }
+
+            }
+
+            if (player.GetComponent<Player>().playerNumber == 3)
+            {
+
+                if (P3timer >= 0.0f)
+                {
+                    P1timer -= Time.deltaTime;
+                    int min = Mathf.FloorToInt(P3timer / 60);
+                    int sec = Mathf.FloorToInt(P3timer % 60);
+
+                }
+                if (P3timer <= 0.0f)
+                {
+                    P3timer = originalTimer;
+                    GameManager.GetComponent<ROTHManager>().P3AddScore(pointValue);
+                }
+
+            }
+
+            if (player.GetComponent<Player>().playerNumber == 4)
+            {
+
+                if (P4timer >= 0.0f)
+                {
+                    P1timer -= Time.deltaTime;
+                    int min = Mathf.FloorToInt(P4timer / 60);
+                    int sec = Mathf.FloorToInt(P4timer % 60);
+
+                }
+                if (P4timer <= 0.0f)
+                {
+                    P1timer = originalTimer;
+                    GameManager.GetComponent<ROTHManager>().P4AddScore(pointValue);
+                }
+
+            }
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "RagDoll")
+        {
+            if (player.GetComponent<Player>().playerNumber == 1)
+            {
+                P1timer = originalTimer;
+            }
+
+            if (player.GetComponent<Player>().playerNumber == 2)
+            {
+                P3timer = originalTimer;
+            }
+
+            if (player.GetComponent<Player>().playerNumber == 3)
+            {
+                P3timer = originalTimer;
+            }
+
+            if (player.GetComponent<Player>().playerNumber == 4)
+            {
+                P4timer = originalTimer;
             }
         }
     }
