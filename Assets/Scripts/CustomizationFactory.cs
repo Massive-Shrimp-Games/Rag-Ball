39a0062f3277608b6eacc83cad41a0414608b591
Abrams <ata57@drexel.edu>
2020-02-26 14:46:54 -0500

    Started adding fan instantiation for customization. needs to have lever and fan connected in scene so it can turn on and off

---
+++ Assets/Scripts/CustomizationFactory.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CustomizationFactory : MonoBehaviour
+{
+    public bool FanEnabled;
+
+    public GameObject Fan_Spinner;
+    public GameObject Fan_Lever;
+    
+    public Transform[] spawnPoints;
+
+    private void Start()
+    {
+        if (FanEnabled == true)
+        {
+            GameObject fan = Instantiate(Fan_Spinner);
+            GameObject fan_lever = Instantiate(Fan_Lever);
+            fan.transform.position = spawnPoints[0].position;
+            fan_lever.transform.position = spawnPoints[1].position;
+        }
+    }
+}
