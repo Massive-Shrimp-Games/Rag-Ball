648d708b8b882bcede93b753ea0564314fa993ab
Ranson <jrr334@drexel.edu>
2020-02-28 19:30:07 -0500

    Added options slider scripts

---
+++ Assets/Scripts/GUI/Menus/Options/MenuItems/JumpHeightSlider.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class JumpHeightSlider : HorizontalSlider
+{
+    protected override void Increment(InputValue inputValue)
+    {
+
+    }
+    
+    protected override void Decrement(InputValue inputValue)
+    {
+
+    }
+}
---
+++ Assets/Scripts/GUI/Menus/Options/MenuItems/StaminaRegenSlider.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class StaminaRegenSlider : HorizontalSlider
+{
+    protected override void Increment(InputValue inputValue)
+    {
+
+    }
+    
+    protected override void Decrement(InputValue inputValue)
+    {
+
+    }
+}
