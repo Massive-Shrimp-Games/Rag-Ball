a0b17f5d6e44ef76149202f4f2d237b817970b1c
Ranson <jrr334@drexel.edu>
2020-02-28 19:31:33 -0500

    Wired up options button

--- Assets/Scripts/GUI/Menus/Pause/MenuItems/OptionsButton.cs
+++ Assets/Scripts/GUI/Menus/Pause/MenuItems/OptionsButton.cs
@@ -4,8 +4,11 @@ using UnityEngine;
 
 public class OptionsButton : Button
 {
+    public GameObject optionsScreen;
+
     public override void Select(PlayerCursor cursor)
     {
-        Debug.Log("Button Options");
+        optionsScreen.SetActive(true);
+        cursor.currentMenuItem = optionsScreen.transform.GetChild(1).GetComponent<MenuItem>();
     }
 }
