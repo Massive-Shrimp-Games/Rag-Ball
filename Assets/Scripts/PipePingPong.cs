7126a463f9ede4de27c698796065c13c72e977f0
Mangelsdorf <pjm349@drexel.edu>
2020-03-06 16:25:15 -0500

    Add Pipe Mover

---
+++ Assets/Scripts/PipePingPong.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// https://github.com/peter201943/Rag-Ball-Historic/blob/Multiplayer-Work/Assets/_RagBall/Scripts/PipePingPong.cs
+
+public class PipePingPong : MonoBehaviour
+{
+    public float min;
+    public float max;
+    public float speed = 2f;
+    [SerializeField] private bool PingPongAble = false;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (PingPongAble)
+        { 
+            transform.position = new Vector3(transform.position.x, transform.position.y, Mathf.PingPong(Time.time * speed, max - min) + min);
+        }
+    }
+}
