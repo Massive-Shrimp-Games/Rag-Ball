46c2fd868e0dfab24d694a7af10b68b358f99237
Pierce <ccp49@drexel.edu>
2020-03-15 18:41:23 -0400

    Fixed Music double on menu

--- Assets/GUI/Menus/Title/TitleScreen.cs
+++ Assets/GUI/Menus/Title/TitleScreen.cs
@@ -43,6 +43,7 @@ public class TitleScreen : MonoBehaviour
 
     private void OnStart(InputValue inputValue)
     {
+        Game.Instance.Music.StopAudio();
         SceneManager.LoadScene("MainMenu");
     }
 
